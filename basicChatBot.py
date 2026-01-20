2b1f91e16905e995940370f0c0d56b398f16df86
Ridwan <rhaque34@gatech.edu>
2026-01-19 23:57:00 -0500

    added memory api

--- basicChatBot.py
+++ basicChatBot.py
@@ -7,14 +7,11 @@ from mem0 import MemoryClient
 load_dotenv()
 client = OpenAI()  # Uses OPENAI_API_KEY from .env
 mem0_api_key = os.getenv("MEM0_API_KEY") # Uses MEM0_API_KEY from .env
-memory_client = MemoryClient(api_key=mem0_api_key)
+memory = MemoryClient(api_key=mem0_api_key)
 
-messages = [
-    {
-        "role": "system",
-        "content": "be truthful and engaging in your response",
-    },
-]
+user_id = "avb"
+
+messages = []
 
 while True:
     user_input = input("User: ")
@@ -26,8 +23,24 @@ while True:
         }
     )
     
+    related_memories = memory.search(user_input, user_id=user_id, filters={"user_id": user_id})
+    print(related_memories)
+    
+    related_memories_text = "\n".join([m['memory'] for m in related_memories['results']]) if related_memories['results'] else "No previous interactions found."
+    
+    print(related_memories_text)
+    
+    system_message = [
+        {
+            "role": "system",
+            "content": f"""answer the user's question honestly. 
+Here are some relevant information you may find useful that previous interactions have taught us: 
+{related_memories_text}"""
+        }
+    ]
+    
     response = client.chat.completions.create(
-        messages=messages,
+        messages=system_message + messages,
         model="gpt-4o",
     )
     
@@ -41,5 +54,9 @@ while True:
     )
     
     print(f"\nAssistant: {answer}\n")
+    
+    memory.add(messages[-2:], user_id=user_id)
+    
+    memory.add(messages[-2:], user_id=user_id)
 
 
